653cd409722717beb721c8ce29170320e12846ae
memcmahon <mcmahon.meganelizabeth@gmail.com>
2020-05-28 10:03:59 -0600

    add draw_lotto_winner test

--- test/museum_test.rb
+++ test/museum_test.rb
@@ -122,5 +122,34 @@ class MuseumTest < Minitest::Test
     dmns.admit(patron_3)
 
     assert_equal [patron_1, patron_3], dmns.ticket_lottery_contestants(dead_sea_scrolls)
+    assert_equal [], dmns.ticket_lottery_contestants(gems_and_minerals)
+  end
+
+  def test_it_can_draw_lotto_winner
+    dmns = Museum.new("Denver Museum of Nature and Science")
+    gems_and_minerals = Exhibit.new({name: "Gems and Minerals", cost: 0})
+    dead_sea_scrolls = Exhibit.new({name: "Dead Sea Scrolls", cost: 10})
+    imax = Exhibit.new({name: "IMAX",cost: 15})
+    patron_1 = Patron.new("Bob", 0)
+    patron_2 = Patron.new("Sally", 20)
+    patron_3 = Patron.new("Johnny", 5)
+    dmns.add_exhibit(gems_and_minerals)
+    dmns.add_exhibit(dead_sea_scrolls)
+    dmns.add_exhibit(imax)
+    patron_1.add_interest("Gems and Minerals")
+    patron_1.add_interest("Dead Sea Scrolls")
+    patron_2.add_interest("Dead Sea Scrolls")
+    patron_3.add_interest("Dead Sea Scrolls")
+    dmns.admit(patron_1)
+    dmns.admit(patron_2)
+    dmns.admit(patron_3)
+
+    lotto_contestants = [patron_1.name, patron_3.name]
+    lotto_winner = dmns.draw_lottery_winner(dead_sea_scrolls)
+
+    assert_equal true, dmns.draw_lottery_winner(dead_sea_scrolls) == 'Bob' || dmns.draw_lottery_winner(dead_sea_scrolls) == 'Johnny'
+    assert_equal true, lotto_contestants.include?(lotto_winner)
+    assert_equal false, lotto_contestants.include?(patron_2.name)
+    assert_nil dmns.draw_lottery_winner(imax)
   end
 end
