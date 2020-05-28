40cede18ddc7e24fc5208fb04add59be60238a5c
memcmahon <mcmahon.meganelizabeth@gmail.com>
2020-05-28 09:21:43 -0600

    Add #patrons method

--- lib/museum.rb
+++ lib/museum.rb
@@ -1,10 +1,12 @@
 class Museum
   attr_reader :name,
-              :exhibits
+              :exhibits,
+              :patrons
 
   def initialize(name)
     @name = name
     @exhibits = []
+    @patrons = []
   end
 
   def add_exhibit(exhibit)
@@ -12,6 +14,22 @@ class Museum
   end
 
   def recommend_exhibits(patron)
+    # recommendation = []
+    # @exhibits.each do |exhibit|
+    #   patron.interests.each do |interest|
+    #     if exhibit.name == interest
+    #       recommendation << exhibit
+    #     end
+    #   end
+    # end
+    # recommendation
+    #
+    # @exhibits.find_all do |exhibit|
+    #   patron.interests.any? do |interest|
+    #     exhibit.name == interest
+    #   end
+    # end
+
     @exhibits.find_all do |exhibit|
       patron.interests.include?(exhibit.name)
     end
